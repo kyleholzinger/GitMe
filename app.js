8ded19a84e7c3dc773e6d1635b119b6a18919a6a
Kyle Holzinger <kylelholzinger@gmail.com>
2017-03-27 19:33:28 -0400

    [KH] server

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-import express from "express";
+var express = require("express");
 app = express();
 
 app.use("/", express.static(__dirname))
+
+app.listen(8080, () => console.log("Listening on port 8080"))
